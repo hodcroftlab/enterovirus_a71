f6e1dae546c03235980962fffcb93f9786cc016d
Nadia Neuner-Jehle <nadia.neunerjehle@gmail.com>
2025-02-21 11:06:45 +0100

    Enhance accession number validation with retry logic and rate limiting

--- scripts/check_accession.py
+++ scripts/check_accession.py
@@ -5,69 +5,84 @@ import pandas as pd
 from dotenv import load_dotenv, find_dotenv
 import os
 
+import time
+
 # Function to check if an accession number is real: it uses the entrez functionality of ncbi
-def extract_accession(name,extract="accession"):
-    
+def extract_accession(name, extract="accession"):
     load_dotenv(find_dotenv())
     Entrez.email = os.environ.get("EMAIL")
-    #email address should be stored in an .env file in the base directory, in the format EMAIL=user@email.com
-
+    # email address should be stored in an .env file in the base directory, in the format EMAIL=user@email.com
+    
     # Define the API endpoint and parameters
     url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi'
     params = {'db': 'nuccore', 'term': name, 'retmode': 'xml'}
     
-    try:
-        # Send request to the API
-        response = requests.get(url, params=params)
-        response.raise_for_status()  # Raise HTTPError for bad responses
+    max_retries = 5
+    backoff_factor = 5 #seconds
 
-        # Check if accession exists
-        if '<Count>0</Count>' in response.text:
-            return False, None
+    time.sleep(1) # for every entry sleep first
 
-        # Extract all IDs from the response
-        ids = re.findall(r'<Id>(\d+)</Id>', response.text)
-        if len(ids)>5:
-            return False, None
-        accessions = []
-        
-        for id_name in ids:
-            # Fetch the sequence record using Entrez
-            handle = Entrez.efetch(db="nucleotide", id=id_name, rettype="gb", retmode="text")
-            seq_record = SeqIO.read(handle, "genbank")
-            handle.close()  # Ensure we close the handle after reading
+    for attempt in range(max_retries):
+        try:
+            # Send request to the API
+            response = requests.get(url, params=params)
+            response.raise_for_status()  # Raise HTTPError for bad responses
 
-            # print(seq_record)
+            # Check if accession exists
+            if '<Count>0</Count>' in response.text:
+                return False, None
 
-            # Check if the description contains "Coxsackievirus A6" or "CVA6"
-            if re.search(r'\b(Coxsackievirus A16|CVA16)\b', seq_record.description, re.IGNORECASE):
-                # Check if the description also contains "VP1" or "complete genome"
-                if re.search(r'\bVP1\b', seq_record.description, re.IGNORECASE) or re.search(r'\bcomplete \b', seq_record.description, re.IGNORECASE) or re.search(r'\bpartial cds\b', seq_record.description, re.IGNORECASE):
-                    # what to extract
-                    if extract == "accession":
-                        accession = seq_record.name
-                    elif extract == "strain":
-                        match = re.search(r'(strain|isolate)\s+([\w\-\.\/]+)', seq_record.description)
-                        if match:
-                            strain_name = match.group(2)
-                            if re.search(r'\bcomplete \b', seq_record.description, re.IGNORECASE) and len(ids)>1:
-                                accession = strain_name+"c"
-                            else:
-                                accession = strain_name
-                    else:
-                        accession = seq_record.description
-                    accessions.append(accession)
+            # Extract all IDs from the response
+            ids = re.findall(r'<Id>(\d+)</Id>', response.text)
+            if len(ids) > 5:
+                return False, None
+            accessions = []
 
-        if accessions:
-            # Safely unpack accessions if it has at least one item
-            accession = accessions[0]
-            return True, accession
-        else:
-            return False, None
+            for id_name in ids:
+                # Fetch the sequence record using Entrez
+                handle = Entrez.efetch(db="nucleotide", id=id_name, rettype="gb", retmode="text")
+                seq_record = SeqIO.read(handle, "genbank")
+                handle.close()  # Ensure we close the handle after reading
 
-    except Exception as e:
-        # Handle exceptions gracefully
-        print(f"Error checking accession {name}: {e}")
+                # Check if the description contains "Enterovirus A71" or "EV-A71"
+                if re.search(r'\b(Enterovirus A71|EV-A71)\b', seq_record.description, re.IGNORECASE):
+                    # Check if the description also contains "VP1" or "complete genome"
+                    if re.search(r'\bVP1\b', seq_record.description, re.IGNORECASE) or re.search(r'\bcomplete \b', seq_record.description, re.IGNORECASE) or re.search(r'\bpartial cds\b', seq_record.description, re.IGNORECASE):
+                        # what to extract
+                        if extract == "accession":
+                            accession = seq_record.name
+                        elif extract == "strain":
+                            match = re.search(r'(strain|isolate)\s+([\w\-\.\/]+)', seq_record.description)
+                            if match:
+                                strain_name = match.group(2)
+                                if re.search(r'\bcomplete \b', seq_record.description, re.IGNORECASE) and len(ids) > 1:
+                                    accession = strain_name + "c"
+                                else:
+                                    accession = strain_name
+                        else:
+                            accession = seq_record.description
+                        accessions.append(accession)
+
+            if accessions:
+                # Safely unpack accessions if it has at least one item
+                accession = accessions[0]
+                return True, accession
+            else:
+                return False, None
+
+        except requests.exceptions.HTTPError as e:
+            if response.status_code == 429:
+                # Too many requests, wait and retry
+                wait_time = backoff_factor * (2 ** attempt)
+                print(f"Rate limit exceeded. Retrying in {wait_time} seconds...")
+                time.sleep(wait_time)
+            else:
+                print(f"HTTP error occurred: {e}")
+                break
+        except Exception as e:
+            # Handle other exceptions gracefully
+            print(f"Error checking accession {name}: {e}")
+            break
 
     return False, None
 
@@ -84,4 +99,3 @@ def extract_digits(string, n=4, position='first'):
         return digits[-n:] if len(digits) >= n else digits
     else:
         raise ValueError("Position should be 'first' or 'last'")
-    
--- scripts/update_sequences.py
+++ scripts/update_sequences.py
@@ -5,16 +5,96 @@ import os
 import re
 import time
 from datetime import datetime
-import ipdb
+from check_accession import extract_accession # Function to check if an accession number is real: it uses the entrez functionality of ncbi
 
-# Function to check if an accession number is real: it uses the entrez functionality of ncbi
-from check_accession import extract_accession
+def load_dataframe(file_path, columns):
+    """Load a DataFrame if the file exists, otherwise create an empty one."""
+    if os.path.exists(file_path):
+        return pd.read_csv(file_path, sep='\t')
+    else:
+        return pd.DataFrame(columns=columns)
 
-# Function to extract the accession number from a FASTA header
+# Function to check accession number from a FASTA header
 def get_accession(header):
+    """Extract accession number from a FASTA header."""
     match = re.search(r'\b[A-Z]{1,2}\d{5,6}\b', header)
-    return match.group(0) if match else header.strip()
-  
+    return match.group(0) if match else  header.strip()
+
+
+def update_date(file_path, new_accessions, current_date):
+    """Update date_last_updated file with new accessions."""
+    df_dates = load_dataframe(file_path, ['accession', 'date_added'])
+
+    # Filter out accessions already present
+    new_entries = [
+        {'accession': acc, 'date_added': current_date}
+        for acc in new_accessions if acc not in df_dates['accession'].values
+    ]
+
+    # Append new entries and remove duplicates
+    if new_entries:
+        df_dates = pd.concat([df_dates, pd.DataFrame(new_entries)]).drop_duplicates(subset='accession', keep='first')
+
+    # Save the updated file
+    df_dates.to_csv(file_path, sep='\t', index=False, header=True)
+    print(f"Updated {file_path} with {len(new_entries)} new entries.")
+
+
+def internal_accession(local_df, must_have_local, current_date):
+    """Add new local accessions and check if old ids now on Genbank"""
+
+    # Determine the maximum existing internal accession number
+    if not local_df.empty:
+        existing_ids = local_df['internal_accession'].str.extract(r'(\d+)', expand=False).fillna(0).astype(int)
+        max_id = existing_ids.max() if not existing_ids.empty else 0
+    else:
+        max_id = 0
+
+    # Add a row with the new internal accession
+    for accession in must_have_local:
+        if not re.match(r'^[A-Z]{1,2}\d{5,6}$', accession):
+            max_id += 1
+            new_internal_accession = f"X{max_id:07d}"
+            local_df = pd.concat([local_df, pd.DataFrame([[new_internal_accession, accession, 'NA', current_date, new_internal_accession]], 
+                                                                         columns=['internal_accession', 'sample_name', 'gb_accession', 'date_added', 'seq_accession'])])
+            print(f"Created new internal accession: {new_internal_accession} for {accession}")
+
+    # Replace all invalid accessions in gb_accession column to NA
+    local_df["gb_accession"] = local_df["gb_accession"].apply(lambda x: 'NA' if not re.match(r'^[A-Z]{1,2}\d{5,6}$', x) else x)
+    
+    # Check if any of the samples with internal accessions now have a GenBank accession
+    for index, row in local_df.iterrows():
+        if row['gb_accession'] == 'NA':
+            is_real, gb_accession = extract_accession(row['sample_name'])
+            if is_real:
+                local_df.at[index, 'gb_accession'] = gb_accession
+                local_df.at[index, 'seq_accession'] = gb_accession     # Update seq_accession based on gb_accession
+
+                print(f"Updated {row['sample_name']} with GenBank accession: {gb_accession}")    
+
+    # Remove NA sample names and keep only last occurence
+    local_df = local_df.dropna(subset='sample_name')
+    local_df = local_df.drop_duplicates(subset='sample_name', keep='last')
+
+    return local_df
+
+
+
+def process_fasta_headers(fasta_file, existing_accessions):
+    """Extract new sequence names from a FASTA file."""
+    new_accessions = set()
+
+    with open(fasta_file, 'r') as infile:
+        for line in infile:
+            if line.startswith('>'):
+                header = line[1:].strip()
+                accession = get_accession(header)
+                if accession and accession not in existing_accessions:
+                    new_accessions.add(accession)
+        
+    return new_accessions
+
+ 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         prog='update_sequences',
@@ -29,6 +109,7 @@ if __name__ == '__main__':
     parser.add_argument('--local_accession', help="Local accession file")
     parser.add_argument('--add_metadata', help="Additional extended metadata file")
     parser.add_argument('--metadata', help="metadata file")
+    parser.add_argument('--ingest_seqs', help = "original sequences from ingest")
     args = parser.parse_args()
 
     input_sequences = args.in_seq
@@ -36,176 +117,58 @@ if __name__ == '__main__':
     date_last_updated_file = args.dates
     local_accn_file = args.local_accession
     extended_meta_file = args.add_metadata
-
+    sequences_ingest = args.ingest_seqs
 
     # Get current date
     current_date = datetime.now().strftime("%Y-%m-%d")
 
     # Get download date for sequences
-    t=time.ctime(os.path.getctime("data/sequences.fasta"))
-    seqs_download_date=datetime.strptime(t, "%a %b %d %H:%M:%S %Y").strftime("%d-%m-%Y")
+    t=time.ctime(os.path.getctime(sequences_ingest))
+    seqs_download_date=datetime.strptime(t, "%a %b %d %H:%M:%S %Y").strftime("%Y-%m-%d")
 
-    # read table with extended (published) metadata
-    extended_meta = pd.read_csv(extended_meta_file, sep='\t')
-    extended_meta = extended_meta.loc[~extended_meta.accession.isna(), ['accession', 'strain']]
+    # Load existing accessions from date_last_updated_file
+    existing_accessions = set(
+        load_dataframe(date_last_updated_file, ['accession', 'date_added'])['accession'].values
+    )
+    print("number of accessions:", len(existing_accessions))
 
-    # Read existing date_last_updated.txt file
-    if os.path.exists(date_last_updated_file):
-        df_dates = pd.read_csv(date_last_updated_file, sep='\t')
-    else:
-        df_dates = pd.DataFrame(columns=['accession', 'date_added'])
+    # Load Fasta file headers and get new sequences
+    new_accessions = set(process_fasta_headers(input_sequences,existing_accessions))
+    print("new accessions:", len(new_accessions))
 
-    # Read existing local_accn.txt file
-    if os.path.exists(local_accn_file):
-        df_local_accn = pd.read_csv(local_accn_file, sep='\t',)
-    else:
-        df_local_accn = pd.DataFrame(columns=['internal_accession', 'sample_name', 'gb_accession', 'date_added'])
-
+    # Load local_accession file
+    local_df = load_dataframe(local_accn_file, ["internal_accession","sample_name","gb_accession","date_added","seq_accession"])
     
+    # Check if any of new_accessions is in local_df; if yes remove from new_accessions
+    new_accessions = [accession for accession in new_accessions if accession not in local_df['sample_name'].values and accession not in local_df["seq_accession"].values]
+    print("new accessions after removing local ones:", len(new_accessions))
 
-    # Add entries for accessions or strain names present in the extended_meta
-    for index, row in extended_meta.iterrows():
-        accession, strain = row['accession'], row['strain']
+    if len(new_accessions) > 0:
+        # Check if real accession number, if not, add to must_have_local set
+        must_have_local = set()
+        for accession in new_accessions:
+            if not re.match(r'^[A-Z]{1,2}\d{5,6}$', accession):
+                must_have_local.add(accession)
+
+        print(f"Accessions needing local IDs: {len(must_have_local)}")
+
+        # update local_accn_file with new local ids and check old ids if now on Genbank
+        local_df = internal_accession(local_df, must_have_local, seqs_download_date)
         
-        # Check if the accession or strain is already present
-        if strain in df_local_accn['sample_name'].values and accession not in df_local_accn['gb_accession'].values:
-            old_internal_accession = df_local_accn.loc[df_local_accn['sample_name'] == strain, 'internal_accession'].values[0] # get interal_accession from strain
-            df_local_accn = pd.concat([df_local_accn, pd.DataFrame([[old_internal_accession, strain, accession, current_date]], columns=['internal_accession', 'sample_name', 'gb_accession', 'date_added'])])
-            df_dates = pd.concat([df_dates, pd.DataFrame([[accession, current_date]], columns=['accession', 'date_added'])])
+        local_df.to_csv(local_accn_file, sep='\t', index=False, header=True)
 
-    # remove duplicated accessions or duplicated strain names
-    df_local_accn = df_local_accn.drop_duplicates(subset=['internal_accession', 'sample_name'], keep='last')
-    df_dates = df_dates.drop_duplicates(subset='accession', keep='last')
+    # Create a dictionary from local_df with sample_name as key and seq_accession as value
+    name_to_internal_accn = local_df.set_index('sample_name')['seq_accession'].to_dict()
 
-    # Step 1: Update date_last_updated.txt
-    # Extract sequence names from the input FASTA file
-    sequence_names = set()
-    not_accession = set()
-    local_accn_dic = {}
-    with open(input_sequences, 'r') as seq_file:
-        for line in seq_file:
-            if line.startswith('>'):
-                sequence_name = line[1:].strip()
-                # Check if the sequence is new
-                if sequence_name in df_dates['accession'].values or sequence_name in df_local_accn['sample_name'].values:
-                    continue  # Skip this sequence if it's already in either the date or local accession files
-                print(sequence_name)
-                if sequence_name in extended_meta.strain:
-                    continue
-                # Proceed with processing for new sequences
-                if '|' in sequence_name:
-                    local_accn_dic[sequence_name] = get_accession(sequence_name)
-                    sequence_name = get_accession(sequence_name)
+    # Replace the sample_name in new_accessions with the seq_accession
+    updated_new_accessions = [name_to_internal_accn.get(accession, accession) for accession in new_accessions]
 
-                # Directly consider it a real accession if it matches the pattern
-                elif re.match(r'^[A-Z]{2}\d{6}$', sequence_name): 
-                    pass
+    # Update date_last_updated_file with the updated new_accessions list
+    update_date(date_last_updated_file, updated_new_accessions, seqs_download_date)
 
-                else:
-                    is_real, accession = extract_accession(sequence_name)
-                    
-                    if is_real and not re.match(r'^[A-Z]{2}\d{6}$', accession):
-                        is_real = False
-                    if not is_real:
-                        not_accession.add(sequence_name)
-                        sequence_name = None
-                    elif accession:
-                        local_accn_dic[sequence_name] = accession
-                        sequence_name = accession
-                        
-                if sequence_name:
-                    sequence_names.add(sequence_name)
+    print("number of accessions after replacing local ones:", len(updated_new_accessions))
 
-
-    # Update df_dates DataFrame
-    new_entries = pd.DataFrame({'accession': list(sequence_names), 'date_added': current_date})
-    df_dates = pd.concat([df_dates, new_entries]).drop_duplicates(subset='accession', keep='last')
-    df_dates=df_dates.reset_index(drop=True)
-
-
-    # Save updated date_last_updated.txt file; further down now
-    # df_dates.to_csv(date_last_updated_file, sep='\t', index=False, header=True)
-
-    # Step 2: Update local_accn.txt
-    # Determine the maximum existing internal accession number
-    if not df_local_accn.empty:
-        # Replace NaN values with a placeholder before converting to integers
-        existing_ids = df_local_accn['internal_accession'].str.extract(r'(\d+)', expand=False).fillna(0).astype(int)
-        max_id = existing_ids.max() if not existing_ids.empty else 0
-    else:
-        max_id = 0
-
-
-    # Create DataFrame for new entries
-    new_entries = []
-    if not not_accession:
-        for name in not_accession:
-            if name not in df_local_accn['sample_name'].values:
-                # Assuming typical GenBank accession
-                if len(name) == 8 and name[:2].isalpha() and name[2:].isdigit():  
-                    gb_accession = name
-                else:
-                    max_id += 1
-                    new_internal_accession = f"X{max_id:07d}"
-                    gb_accession = 'NA'
-                    new_entries.append([new_internal_accession, name, gb_accession, current_date])
-
-    for name in local_accn_dic.keys():
-        if re.match(r'^[A-Z]{2}\d{6}$', name):
-            local_accn_dic[name] = None
-
-
-    df = pd.DataFrame(local_accn_dic.items(), columns=['sample_name', 'gb_accession'])
-    df=df.loc[df['gb_accession'].apply(lambda x: len(x) == 8 and x[:2].isalpha() and x[2:].isdigit()), :]
-    df['date_added'] = current_date
-
-    df_new_local_accn = pd.DataFrame(new_entries, columns=['internal_accession', 'sample_name', 'gb_accession', 'date_added'])
-
-    # Concatenate dataframes
-    df_local_accn = pd.concat([df_local_accn, df_new_local_accn, df])
-
-    # Drop rows where all columns except 'sample_name' are NA
-    df_local_accn = df_local_accn.dropna(subset='sample_name')
-
-    # Then drop duplicates based on 'sample_name', keeping the last occurrence
-    df_local_accn = df_local_accn.drop_duplicates(subset='sample_name', keep='last')
-
-    # Define a lambda function to select 'gb_accession' if it is not NA, otherwise 'internal_accession'
-    df_local_accn['seq_accession'] = df_local_accn.apply(
-        lambda row: row['gb_accession'] if pd.notna(row['gb_accession']) else row['internal_accession'],
-        axis=1)
-
-
-    # Save updated local_accn.txt file
-    df_local_accn.to_csv(local_accn_file, sep='\t', index=False, header=True)
-
-
-    # Step 3: Replace headers in the sequences and save to output file
-    # Create a mapping from sample_name to internal_accession
-
-    # open date_last_updated_file and add new names
-    df=df_local_accn.loc[:, ["seq_accession","date_added"]].reset_index(drop=True)
-    df=df.rename(columns={"seq_accession":"accession"})
-    df_dates=df_dates.reset_index(drop=True)
-
-    # merge them
-    df_dates_c = pd.concat([df_dates, df]).drop_duplicates(subset='accession', keep='last')
-
-    # Read metadata
-    meta = pd.read_csv(args.metadata, sep='\t')
-
-    # Drop duplicates and convert to DataFrame
-    accn_list = meta[['accession']].drop_duplicates()
-    accn_list['date_added'] = seqs_download_date
-
-    # Concatenate with the existing DataFrame
-    df_dates_c = pd.concat([df_dates_c, accn_list]).sort_values(by="date_added", ascending=True).drop_duplicates(subset='accession', keep='last')
-    df_dates_c.to_csv(date_last_updated_file, sep='\t', index=None)
-
-    # Create the dictionary
-    name_to_internal_accn = df_local_accn.set_index('sample_name')['seq_accession'].to_dict()
-
-    # Replace the headers in fasta
+    # Replace the headers in fasta with the new accession (in replacement_df)
     with open(input_sequences, 'r') as infile, open(output_sequences, 'w') as outfile:
         for line in infile:
             if line.startswith('>'):
